1a09d13934a458e99cb6958ca0d077c9dd374db6
Val-Potitova <71068874+Val-Potitova@users.noreply.github.com>
2023-08-27 18:23:25 +0300

    hello js

---
+++ js_syntax/js/script.js
@@ -0,0 +1,55 @@
+// SYNTAX
+
+// console.log('JS');
+// console.log('fgf')
+// console.log('JS')
+// console.log('fr')
+
+// console.log('rg') console.log('gtr')
+// error
+
+// console.log('dg');
+// console.log('fkgn');
+
+
+
+// CONST
+
+// let colorEyes;
+
+// let user;
+// let userName;
+// userName = 'Vasya';
+// user = userName;
+// console.log(user);
+
+// let user = 'dg' let age = 36
+// error
+
+// let myage = 36;
+// const BLOCK_SIZE = 14 + 50;
+// const BLOCK_HEIGHT = 100;
+// if (true) {
+//     var size = 15;
+// }
+// console.log(size);
+
+
+// DATA TYPE
+
+let userAge = 36;
+let userInfo = 'Freelance ${userAge}';
+console.log(userInfo);
+// fail
+
+let userHeight = 145 / 0;
+console.log(userHeight);
+// Infinity
+
+let userName;
+console.log(typeof userName);
+// undefined
+
+let userSize = "45" / "8";
+console.log(typeof userSize);
+// number
